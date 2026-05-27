2a7c11577bc29e8b1335a6e3d57595f2f0745a51
Pontus Johansson <johanssonp998@gmail.com>
2026-05-27 15:52:11 +0200

    uppdatera api för loginvägen, redirect till admin-sidan vid lyckad inloggning

--- src/js/login.js
+++ src/js/login.js
@@ -21,7 +21,7 @@ form.addEventListener("submit", async (e) => {
 
     try {
         // Skickar POST anrop till backend.
-        const response = await fetch(`${API_URL}/login`, {
+        const response = await fetch(`${API_URL}/auth/login`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ username, password })
