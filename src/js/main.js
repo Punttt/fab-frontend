a4b9525b725609706982130f53e91a93b28be618
Pontus Johansson <johanssonp998@gmail.com>
2026-05-28 12:44:53 +0200

    tar bort de statiska mankorten, börjar med api anropen

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+// Importerar API:et
+import { API_URL } from "./api.js";
+
+// Mappning för dagarna till sv
+
+const dayNames =  {
+    monday: "MÅNDAG",
+    tuesday: "TISDAG",
+    wednesday: "ONSDAG",
+    thursday: "TORSDAG",
+    friday: "FREDAG"
+};
+
