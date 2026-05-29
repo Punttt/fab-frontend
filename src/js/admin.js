f94cb0eaad2b78308ba9f3c5726c5f56de0fa04a
Pontus Johansson <johanssonp998@gmail.com>
2026-05-29 13:56:13 +0200

    börjar med eventlyssnare för adminsida

--- src/js/admin.js
+++ src/js/admin.js
@@ -8,8 +8,28 @@ if(!token) {
     window.location.href = "/login";
 }
 
+let currentMenuId = null;
+
 // När token raderat redirect till login-sidan
 document.getElementById("logoutBtn").addEventListener("click", () => {
     sessionStorage.removeItem("token");
     window.location.href = "/login";
 });
+
+// Ladda veckoformuläret
+const weekForm = document.getElementById("weekForm");
+
+// Eventlyssnare för veckoformuläret
+weekForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const week = weekForm.week_number.value;
+    const year = weekForm.year.value;
+
+    try {
+
+    } catch (error) {
+        console.error(error);
+        showMessage("weekStatus", "Kunde inte ansluta till servern", "error");
+    }
+}); 
